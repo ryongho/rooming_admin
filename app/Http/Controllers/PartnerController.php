8231eafda593e32b45a6195af2b7221657f8e753
yongho0720 <roh.yh0720@gmail.com>
2022-03-31 19:59:44 +0900

    회원 리스트 다운로드 추가

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -261,7 +261,7 @@ class PartnerController extends Controller
 
         // Redirect output to a client’s web browser (Excel2007)
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="user_list.xlsx"');
+        header('Content-Disposition: attachment;filename="partner_list.xlsx"');
         header('Cache-Control: max-age=0');
         // If you're serving to IE 9, then the following may be needed
         header('Cache-Control: max-age=1');
